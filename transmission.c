ffd35b8f8938e9a2ec6317efd3538d5efe405b36
Arthur <arthurtoussaint@free.fr>
2011-09-03 09:16:08 +0200

    transmission.c :: dtransmission - Ajout de la réception du nombre de fichiers a copier et du nom du fichier
    transmission.h :
    - Ajout du prototype de la fonction dtransmission

--- transmission.c
+++ transmission.c
@@ -13,11 +13,13 @@ void transmission(SOCKET csock, SOCKET csock2){
 	puts("in transmission");
 	if (recv(csock, (char*)&temp_taille_nom, sizeof(int), 0) == SOCKET_ERROR)
 		printf("temp_taille_nom error");
+        
 	taille_nom = ntohl(temp_taille_nom);
 	printf("taile_nom : %d\n", taille_nom);
 	nom_fichier = malloc(taille_nom);
 	if (recv(csock, nom_fichier, taille_nom * sizeof(char), 0) == SOCKET_ERROR)
-		printf("temp_taille_nom error");
+                printf("temp_taille_nom error");
+        
 	printf("nom : %s\n", nom_fichier);
 
 	send(csock2, (char*)&temp_taille_nom, sizeof(int), 0);
@@ -27,19 +29,32 @@ void transmission(SOCKET csock, SOCKET csock2){
 	send(csock, &fin, sizeof(char), 0);
 
 	do {
-		recv(csock, (char*)&temp_test_fin, sizeof(int), 0);
-		recv(csock, (char*)buffer, 1024, 0);
+                recv(csock, (char*)&temp_test_fin, sizeof(int), 0);
+                recv(csock, (char*)buffer, 1024, 0);
 		test_fin = ntohl(temp_test_fin);
 
-		send(csock2, (char*)&temp_test_fin, sizeof(int), 0);
-		send(csock2, (char*)buffer, 1024, 0);
-		
-		recv(csock2, &fin, sizeof(char), 0);
+                send(csock2, (char*)&temp_test_fin, sizeof(int), 0);
+                send(csock2, (char*)buffer, 1024, 0);
+       
+                recv(csock2, &fin, sizeof(char), 0);
 		send(csock, &fin, sizeof(char), 0);
 	} while (test_fin == 1024);
 }
 
 
 void dtransmission(SOCKET csock, SOCKET csock2){
-    int temp_taille_chaine;
+    int net_taille_chaine, nb_fichiers, net_nb_fichiers, taille_chaine, i;
+    char *nom_fichier = NULL;
+    
+    
+    recv(csock, &net_nb_fichiers, sizeof(int), 0);
+    nb_fichiers = ntohl(net_nb_fichiers);
+    
+    for (i = 1 ; i < nb_fichiers ; i++){
+        recv(csock, &net_taille_chaine, sizeof(int), 0);    
+        taille_chaine = ntohl(net_taille_chaine);
+        nom_fichier = malloc(taille_chaine);
+        recv(csock, nom_fichier, taille_chaine, 0);
+    }
+
 }
--- transmission.h
+++ transmission.h
@@ -1,3 +1,4 @@
 #include "socket.h"
 
 void transmission(SOCKET csock, SOCKET csock2);
+void dtransmission(SOCKET csock, SOCKET csock2);
