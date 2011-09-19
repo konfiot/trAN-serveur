5333c0be11b34c6015202d18c32a310e32993c22
Arthur <arthurtoussaint@free.fr>
2011-09-19 08:25:03 +0200

    Ajout de la branche Multithreading qui contient l'implémentation du multithreading côté serveur.
    Ajout de structures.h qui contient les structures nécassitées par le multithreading.
    
    structures.h :
    - Ajout de la strucure CoupleClients, qui contient les 2 sockets client

---
+++ structures.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   structures.h
+ * Author: TOUSSAINT
+ *
+ * Created on 19 septembre 2011, 08:19
+ */
+
+#ifndef STRUCTURES_H
+#define	STRUCTURES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+#include "socket.h"
+
+typedef struct {
+    SOCKET csock;
+    SOCKET csock2;
+} CoupleClients;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* STRUCTURES_H */
+
